68d6fc32f842a9801a339f54273ce2daf367ab8b
pikachu0310 <sunnon@i.softbank.jp>
2023-03-29 19:41:39 +0900

    discordBOTの枠組み完成

--- main.go
+++ main.go
@@ -1,1 +1,53 @@
-package pikaGPT
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func GetToken() (apiKey string) {
+	var err = godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("error: tokenが読み込めなかった!: %v", err)
+	}
+	apiKey = os.Getenv("TOKEN")
+	return apiKey
+}
+
+var BotToken = GetToken()
+
+func main() {
+	discord, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		panic(err)
+	}
+
+	discord.AddHandler(onMessageCreate)
+
+	err = discord.Open()
+
+	stopBot := make(chan os.Signal, 1)
+
+	signal.Notify(stopBot, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	<-stopBot
+
+	err = discord.Close()
+
+	return
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	fmt.Printf("Message: %s, Author: %s", m.Message.Content, m.Author.Username)
+	fmt.Println(m.Message)
+	if m.Message.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
